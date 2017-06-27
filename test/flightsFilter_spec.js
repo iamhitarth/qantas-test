f881a3d21f4a300045186a839b4bb528583de48a
Hitarth Sharma <hitarth.sharma@outlook.com>
2017-06-27 16:58:24 +1200

    Added a test spec for the flight filter lib.

---
+++ test/flightsFilter_spec.js
@@ -0,0 +1,10 @@
+let assert = require('assert');
+
+describe('Flight filter will return', () => {
+
+  it('JSON object with flights key with array of flights.', () => {});
+
+  it('a filtered list of flights.', () => {});
+
+  it('just the expected fields.', () => {});
+});
